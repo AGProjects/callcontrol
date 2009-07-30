527b524dda6f4dd552dd68aa269675d284434148
Luci Stanescu <luci@ag-projects.com>
2009-07-30 12:30:20 +0000

    Added dependency checking using application.dependency

--- callcontrol/__init__.py
+++ callcontrol/__init__.py
@@ -7,3 +7,19 @@ __version__ = "2.0.5"
 
 configuration_filename = 'config.ini'
 backup_calls_file = 'calls.dat'
+
+package_requirements = {'python-application': '1.1.5'}
+
+try:
+    from application.dependency import ApplicationDependencies
+except:
+    class DependencyError(Exception): pass
+    class ApplicationDependencies(object):
+        def __init__(self, *args, **kwargs):
+            pass
+        def check(self):
+            raise DependencyError("need python-application version %s or higher but it's not installed" % requirements['python-application'])
+
+dependencies = ApplicationDependencies(**package_requirements)
+
+
