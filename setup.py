4bd072a0596a0a0c750771c2ef22e7be81edc23f
Dan Pascu <dan@ag-projects.com>
2019-08-13 12:36:05 +0000

    Refactored setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python
 
-import re
+import callcontrol
 from distutils.core import setup
 
 
-def get_version():
-    return re.search(r"""__version__\s+=\s+(?P<quote>['"])(?P<version>.+?)(?P=quote)""", open('callcontrol/__init__.py').read()).group('version')
+setup(
+    name='callcontrol',
+    version=callcontrol.__version__,
 
-setup(name         = "callcontrol",
-      version      = get_version(),
-      author       = "AG Projects",
-      author_email = "support@ag-projects.com",
-      url          = "http://callcontrol.ag-projects.com",
-      description  = "SIP call control",
-      license      = "GPL",
-      platforms    = ["Platform Independent"],
-      classifiers  = [
-          #"Development Status :: 1 - Planning",
-          #"Development Status :: 2 - Pre-Alpha",
-          #"Development Status :: 3 - Alpha",
-          #"Development Status :: 4 - Beta",
-          "Development Status :: 5 - Production/Stable",
-          #"Development Status :: 6 - Mature",
-          #"Development Status :: 7 - Inactive",
-          "Intended Audience :: Service Providers",
-          "License :: GNU General Public License (GPL)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python"
-      ],
-      packages     = ['callcontrol','callcontrol.rating','callcontrol.rating.backends'],
-      scripts      = ['call-control']
+    description='SIP call control engine',
+    long_description='Call control engine for OpenSIPS',
+    url='http://callcontrol.ag-projects.com',
+
+    license='GPLv2',
+    author='AG Projects',
+    author_email='support@ag-projects.com',
+
+    platforms=['Platform Independent'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Service Providers',
+        'License :: GNU General Public License 2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+
+    packages=['callcontrol', 'callcontrol.rating', 'callcontrol.rating.backends'],
+    scripts=['call-control']
 )
